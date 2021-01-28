5af0b8f3b1e8e3667f6bf7c6c80427ba9e3da6ed
George W F Santos <gweverton21@hotmail.com>
2021-01-28 10:48:08 -0300

    add and configure json-server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
+
+interface Product {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <Title>Hello, NextJs Project</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(product => {
+            return (
+              <li key={product.id}>
+                {product.title}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   )
 }
